c6242ec2355676130dce1fe3e20200fe4ad2bb06
Richard Baker <rich@mondaymorning.org>
2015-06-30 15:04:17 +0100

    basic working version (no http header stuff yet)

--- calSummary.php
+++ calSummary.php
@@ -4,20 +4,51 @@ use Sabre\VObject;
 require_once 'vendor/autoload.php';
 
 $calConfig = [
-	'dfrsf4fd' => 'https://www.google.com/calendar/ical/e5dtcl0a60cnlu00ma9mses6sk%40group.calendar.google.com/private-355f178d1421d1d12a94ce6173bde5ec/basic.ics'
+	'dfrsf4fd' => [ 'url' => 'https://www.google.com/calendar/ical/e5dtcl0a60cnlu00ma9mses6sk%40group.calendar.google.com/private-355f178d1421d1d12a94ce6173bde5ec/basic.ics',
+									'range' => 30],
+	'sh4d3t' => [ 'url' => 'https://www.google.com/calendar/ical/e5dtcl0a60cnlu00ma9mses6sk%40group.calendar.google.com/private-355f178d1421d1d12a94ce6173bde5ec/basic.ics',
+														'range' => 90],							
 ];
 
-$cal = VObject\Reader::read(fopen ($calConfig['dfrsf4fd'], 'r'));
+$calURL = $calConfig['sh4d3t']['url'];
+$range = $calConfig['sh4d3t']['range'];
 
+// Get the calendar
+$cal = VObject\Reader::read(fopen ($calURL, 'r'));
+
+// Expand any repeated events in the next 'range' days
+$now = new DateTime();
+$then = new DateTime();
+$then->add(new DateInterval('P' . $range . 'D'));
+
+$cal->expand($now, $then);
+
+// Get the events
 $events = $cal->VEVENT;
-//var_dump ($events);
+
+$results = array ();
 
 foreach($cal->VEVENT as $event) {
-	print($event->SUMMARY) . "\n";
+	$dtstart = $event->DTSTART->getDateTime();
+	$results[] = [ 'date' => $dtstart, 'event' => $event->SUMMARY ];
 }
 
+// Sort by date ascending
+usort($results, "arraySort");
+
+// Display as a single line of text 
+foreach ($results as $event){
+	print "* ";
+	print($event['date']->format('d/m/Y')) . " - ";
+	print($event['event']) . " ";
+}
+
+print "*";
 
 
+function arraySort($a, $b){
+	return ($a['date']->getTimestamp() < $b['date']->getTimestamp()) ? -1 : 1;
+}
 
 
 
